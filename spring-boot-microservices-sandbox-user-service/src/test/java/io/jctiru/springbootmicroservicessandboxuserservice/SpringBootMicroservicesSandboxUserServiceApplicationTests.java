2323801b1f9c2b17587f9bdc45778d1655aa4ba0
jctiru <jonathanchristiantiru@gmail.com>
2020-05-15 23:22:21 +0800

    Initial commit

---
+++ spring-boot-microservices-sandbox-user-service/src/test/java/io/jctiru/springbootmicroservicessandboxuserservice/SpringBootMicroservicesSandboxUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jctiru.springbootmicroservicessandboxuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesSandboxUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
