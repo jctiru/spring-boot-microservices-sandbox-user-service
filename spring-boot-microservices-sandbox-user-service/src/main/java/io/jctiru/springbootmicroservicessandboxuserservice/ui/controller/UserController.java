064e952a075ca27c7e0b57294924105b490ce70e
jctiru <jonathanchristiantiru@gmail.com>
2020-05-20 22:27:39 +0800

    Create user controller

--- spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/ui/controller/UserController.java
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/ui/controller/UserController.java
@@ -1,14 +1,29 @@
 package io.jctiru.springbootmicroservicessandboxuserservice.ui.controller;
 
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.jctiru.springbootmicroservicessandboxuserservice.service.UserService;
+import io.jctiru.springbootmicroservicessandboxuserservice.shared.dto.UserDto;
+import io.jctiru.springbootmicroservicessandboxuserservice.ui.model.request.CreateUserRequestModel;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private ModelMapper modelMapper;
 
 	@Autowired
 	private Environment env;
@@ -16,6 +31,14 @@ public class UserController {
 	@GetMapping("/status/check")
 	public String status() {
 		return "Working on port " + env.getProperty("local.server.port");
+	}
+
+	@PostMapping
+	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+		userService.createUser(userDto);
+
+		return "Create user method";
 	}
 
 }
