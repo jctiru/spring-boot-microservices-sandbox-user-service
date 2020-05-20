615811b2d2379b21f6e558184092422a010a45ce
jctiru <jonathanchristiantiru@gmail.com>
2020-05-20 22:26:17 +0800

    Create bean definitions class

---
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/BeanDefinitions.java
@@ -0,0 +1,19 @@
+package io.jctiru.springbootmicroservicessandboxuserservice;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanDefinitions {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+		return modelMapper;
+	}
+
+}
