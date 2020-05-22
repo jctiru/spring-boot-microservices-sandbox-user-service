9fc6debc89cc93f466ec523ecc3a936eff791dc6
jctiru <jonathanchristiantiru@gmail.com>
2020-05-22 18:42:07 +0800

    Allow only ip address of api gateway to access endpoints

--- spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/security/WebSecurity.java
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/security/WebSecurity.java
@@ -1,5 +1,7 @@
 package io.jctiru.springbootmicroservicessandboxuserservice.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,12 +10,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private Environment env;
+
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 				.authorizeRequests()
-				.antMatchers("/users/**").permitAll()
-				.antMatchers("/h2-console/**").permitAll()
+				.antMatchers("/**").hasIpAddress(env.getProperty("gateway.ip"))
 				.anyRequest().authenticated()
 				.and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
