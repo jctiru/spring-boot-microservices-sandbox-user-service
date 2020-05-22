07ccd735f11cecb7039b4ea35bb7c1831dcbb8de
jctiru <jonathanchristiantiru@gmail.com>
2020-05-22 21:52:24 +0800

    Add findUserByEmail method in UserRepository

--- spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/io/repository/UserRepository.java
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/io/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package io.jctiru.springbootmicroservicessandboxuserservice.io.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import io.jctiru.springbootmicroservicessandboxuserservice.io.entity.UserEntity;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+	UserEntity findUserByEmail(String email);
 
 }
