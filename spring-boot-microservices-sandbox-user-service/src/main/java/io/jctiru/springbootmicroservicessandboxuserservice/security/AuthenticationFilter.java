8d62121e4f92be86c671d988bb204b793226502f
jctiru <jonathanchristiantiru@gmail.com>
2020-05-22 21:54:28 +0800

    Create AuthenticationFilter using JWT

---
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/security/AuthenticationFilter.java
@@ -0,0 +1,77 @@
+package io.jctiru.springbootmicroservicessandboxuserservice.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jctiru.springbootmicroservicessandboxuserservice.service.UserService;
+import io.jctiru.springbootmicroservicessandboxuserservice.shared.dto.UserDto;
+import io.jctiru.springbootmicroservicessandboxuserservice.ui.model.request.LoginRequestModel;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private Environment env;
+
+	@Override
+	@Autowired
+	public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+		super.setAuthenticationManager(authenticationManager);
+
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
+			throws AuthenticationException {
+		try {
+			LoginRequestModel creds = new ObjectMapper().readValue(req.getInputStream(), LoginRequestModel.class);
+
+			return this.getAuthenticationManager().authenticate(
+					new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>()));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+			Authentication auth) throws IOException, ServletException {
+		String userName = ((User) auth.getPrincipal()).getUsername();
+		UserDto userDetails = userService.getUserDetailsByEmail(userName);
+
+		String token = Jwts.builder()
+				.setSubject(userDetails.getUserId())
+				.setExpiration(
+						new Date(System.currentTimeMillis() + Long.parseLong(env.getProperty("token.expiration_time"))))
+				.signWith(SignatureAlgorithm.HS512, env.getProperty("token.secret"))
+				.compact();
+
+		res.setContentType("application/json");
+		res.addHeader("token", token);
+		res.addHeader("userId", userDetails.getUserId());
+	}
+
+}
