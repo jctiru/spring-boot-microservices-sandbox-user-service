5d6115a0eb3033743f3dbae5b0b58c5857dab915
jctiru <jonathanchristiantiru@gmail.com>
2020-05-22 18:51:22 +0800

    Create login request model

---
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/ui/model/request/LoginRequestModel.java
@@ -0,0 +1,33 @@
+package io.jctiru.springbootmicroservicessandboxuserservice.ui.model.request;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class LoginRequestModel {
+
+	@NotBlank(message = "Email must not be empty")
+	@Email(message = "Email must be a valid email address")
+	private String email;
+
+	@NotBlank(message = "Password must not be empty")
+	@Size(min = 6, message = "Password must be at least 6 characters")
+	private String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
