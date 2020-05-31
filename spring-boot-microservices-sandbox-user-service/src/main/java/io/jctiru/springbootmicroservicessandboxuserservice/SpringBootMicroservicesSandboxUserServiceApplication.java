41e2e9653c6ecd26de2a44eb50fe910e743486c0
jctiru <jonathanchristiantiru@gmail.com>
2020-05-31 21:54:43 +0800

    Add spring-cloud-starter-openfeign dependency

--- spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/SpringBootMicroservicesSandboxUserServiceApplication.java
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/SpringBootMicroservicesSandboxUserServiceApplication.java
@@ -3,7 +3,9 @@ package io.jctiru.springbootmicroservicessandboxuserservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SpringBootMicroservicesSandboxUserServiceApplication {
