d450198aa3d0968058e90cba01bf30573203e21e
jctiru <jonathanchristiantiru@gmail.com>
2020-05-31 21:17:51 +0800

    Add rest template bean in BeanDefinitions

--- spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/BeanDefinitions.java
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/BeanDefinitions.java
@@ -2,9 +2,11 @@ package io.jctiru.springbootmicroservicessandboxuserservice;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class BeanDefinitions {
@@ -20,6 +22,12 @@ public class BeanDefinitions {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
