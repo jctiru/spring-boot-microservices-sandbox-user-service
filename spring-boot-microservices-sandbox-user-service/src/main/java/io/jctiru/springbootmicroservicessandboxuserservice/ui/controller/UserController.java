4c23d205592efba57c16b4eca55977a427201cf5
jctiru <jonathanchristiantiru@gmail.com>
2020-05-31 21:19:31 +0800

    Add getUser endpoint

--- spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/ui/controller/UserController.java
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/ui/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import io.jctiru.springbootmicroservicessandboxuserservice.service.UserService;
 import io.jctiru.springbootmicroservicessandboxuserservice.shared.dto.UserDto;
 import io.jctiru.springbootmicroservicessandboxuserservice.ui.model.request.CreateUserRequestModel;
 import io.jctiru.springbootmicroservicessandboxuserservice.ui.model.response.CreateUserResponseModel;
+import io.jctiru.springbootmicroservicessandboxuserservice.ui.model.response.UserResponseModel;
 
 @RestController
 @RequestMapping("/users")
@@ -45,6 +47,15 @@ public class UserController {
 		CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
+	}
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserResponseModel> getUser(@PathVariable("userId") String userId) {
+		
+		UserDto userDto = userService.getUserByUserId(userId);
+		UserResponseModel returnValue = modelMapper.map(userDto, UserResponseModel.class);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(returnValue);
 	}
 
 }
