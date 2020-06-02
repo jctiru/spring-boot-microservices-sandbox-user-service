f2f2ca51b9a738c0d84554d4c34f43d15e755c63
jctiru <jonathanchristiantiru@gmail.com>
2020-06-02 18:49:35 +0800

    Remove unused rest template code

--- spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/BeanDefinitions.java
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/BeanDefinitions.java
@@ -2,11 +2,9 @@ package io.jctiru.springbootmicroservicessandboxuserservice;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.client.RestTemplate;
 
 import feign.Logger;
 import io.jctiru.springbootmicroservicessandboxuserservice.shared.FeignErrorDecoder;
@@ -25,12 +23,6 @@ public class BeanDefinitions {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
 	}
 
 	@Bean
--- spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/service/impl/UserServiceImpl.java
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/service/impl/UserServiceImpl.java
@@ -31,13 +31,8 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-//	@Autowired
-//	private RestTemplate restTemplate;
-
 	@Autowired
 	private AlbumServiceClient albumServiceClient;
-
-//	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
@@ -83,15 +78,7 @@ public class UserServiceImpl implements UserService {
 		}
 
 		UserDto userDto = modelMapper.map(userEntity, UserDto.class);
-
-//		String albumUrl = String.format("http://album-service/users/%s/albums", userId);
-//		ResponseEntity<List<AlbumResponseModel>> albumListResponse = restTemplate.exchange(albumUrl, HttpMethod.GET,
-//				null, new ParameterizedTypeReference<List<AlbumResponseModel>>() {
-//				});
-//		List<AlbumResponseModel> albumList = albumListResponse.getBody();
-
 		List<AlbumResponseModel> albumList = albumServiceClient.getAlbums(userId);
-
 		userDto.setAlbumList(albumList);
 
 		return userDto;
