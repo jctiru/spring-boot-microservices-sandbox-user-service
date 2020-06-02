50f8babacdeb9d4f6feca891e1c7003705409602
jctiru <jonathanchristiantiru@gmail.com>
2020-06-02 18:47:59 +0800

    Add error logging for feign hystrix fallback

--- spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/io/external/AlbumServiceClient.java
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/io/external/AlbumServiceClient.java
@@ -3,14 +3,17 @@ package io.jctiru.springbootmicroservicessandboxuserservice.io.external;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import feign.hystrix.FallbackFactory;
 import io.jctiru.springbootmicroservicessandboxuserservice.ui.model.response.AlbumResponseModel;
 
-@FeignClient(name = "album-service", fallback = AlbumFallback.class)
+@FeignClient(name = "album-service", fallbackFactory = AlbumFallbackFactory.class)
 public interface AlbumServiceClient {
 
 	@GetMapping("/users/{userId}/albumss")
@@ -19,7 +22,22 @@ public interface AlbumServiceClient {
 }
 
 @Component
-class AlbumFallback implements AlbumServiceClient {
+class AlbumFallbackFactory implements FallbackFactory<AlbumServiceClient> {
+
+	@Override
+	public AlbumServiceClient create(Throwable cause) {
+		return new AlbumServiceClientFallback(cause);
+	}
+
+}
+
+class AlbumServiceClientFallback implements AlbumServiceClient {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public AlbumServiceClientFallback(Throwable cause) {
+		logger.error(cause.getLocalizedMessage());
+	}
 
 	@Override
 	public List<AlbumResponseModel> getAlbums(String userId) {
