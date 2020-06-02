3a636741512e00b7fd483005e176d3321d48a6b4
jctiru <jonathanchristiantiru@gmail.com>
2020-06-02 17:17:36 +0800

    Add hystrix fallback for feign client

--- spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/io/external/AlbumServiceClient.java
+++ spring-boot-microservices-sandbox-user-service/src/main/java/io/jctiru/springbootmicroservicessandboxuserservice/io/external/AlbumServiceClient.java
@@ -1,17 +1,29 @@
 package io.jctiru.springbootmicroservicessandboxuserservice.io.external;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import io.jctiru.springbootmicroservicessandboxuserservice.ui.model.response.AlbumResponseModel;
 
-@FeignClient(name = "album-service")
+@FeignClient(name = "album-service", fallback = AlbumFallback.class)
 public interface AlbumServiceClient {
 
 	@GetMapping("/users/{userId}/albumss")
 	public List<AlbumResponseModel> getAlbums(@PathVariable String userId);
+
+}
+
+@Component
+class AlbumFallback implements AlbumServiceClient {
+
+	@Override
+	public List<AlbumResponseModel> getAlbums(String userId) {
+		return new ArrayList<>();
+	}
 
 }
